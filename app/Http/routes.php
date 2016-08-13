2eda1eb70b256ce611e825883e68ab82b9edab92
nicholascm90 <nicholascm90@gmail.com>
2016-08-12 23:52:09 -0400

    minor update to include username on user

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return  view('welcome');
+});
+
+/*
+Route::get('/login', 'Auth\AuthController@getLogin');
+Route::post('/login', 'Auth\AuthController@postLogin');
+Route::get('/logout', 'Auth\AuthController@getLogout');
+
+Route::get('/register', 'Auth\AuthController@getRegister');
+Route::post('/register', 'Auth\AuthController@postRegister');
+*/
+Route::get('/test', function() {
+  return view('welcome');
 });
