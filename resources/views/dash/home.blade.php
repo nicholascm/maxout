9f6a43818b15d0f6ecaee091a95cede24753752d
nicholascm90 <nicholascm90@gmail.com>
2016-08-13 00:45:23 -0400

    views for auth/dash/layout

---
+++ resources/views/dash/home.blade.php
@@ -0,0 +1,17 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+<div class = "col-md-3">
+  Profile Info
+</div>
+
+<div class = "col-md-6">
+  Dashboard
+</div>
+
+<div class = "col-md-3">
+  Followers
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+
+  </head>
+  <body>
+    <div class = "container">
+      <nav class = "navbar navbar-default">
+        Nav
+      </nav>
+    </div>
+
+    @yield('content')
+
+  </body>
+</html>
