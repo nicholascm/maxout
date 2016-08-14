3eed2be425710b9f8d640acc3f015bcc1b9d321e
nicholascm90 <nicholascm90@gmail.com>
2016-08-13 22:07:42 -0400

    removed unused file, modified landing with register button

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -4,6 +4,7 @@
   <div class = "jumbotron text-center">
     <h1>maxout</h1>
     <p>Track your power lifting goals in real time</p>
+    <a class = "btn btn-success" href = {{url('/register')}}>Get Started Today</a>
   </div>
 </div>
 @endsection
